3e933743a0ef9d5e5552ad1e606123dfcbf6870f
huihuicodesoft <836320956@qq.com>
2017-07-11 11:04:39 +0800

    修改相应的格式默认情况

--- src/main/java/cn/com/wh/ring/common/response/ResponseHelper.java
+++ src/main/java/cn/com/wh/ring/common/response/ResponseHelper.java
@@ -4,23 +4,27 @@ package cn.com.wh.ring.common.response;
  * Created by Hui on 2017/7/10.
  */
 public class ResponseHelper {
-    public static <T> Response<T> createSuccessResponse(){
+    public static <T> Response<T> createSuccessResponse() {
         return createSuccessResponse(null);
     }
 
-    public static <T> Response<T> createSuccessResponse(T payload){
-        Response<T>  response = new Response<T>();
+    public static <T> Response<T> createSuccessResponse(T payload) {
+        Response<T> response = new Response<T>();
         response.setCode(ResponseCode.OK);
-        response.setDescription(null);
-        response.setPayload(payload);
+        response.setDescription("");
+        if (payload != null) {
+            response.setPayload(payload);
+        }
         return response;
     }
 
-    public static <T> Response<T> createResponse(int code, String description, T payload){
-        Response<T>  response = new Response<T>();
+    public static <T> Response<T> createResponse(int code, String description, T payload) {
+        Response<T> response = new Response<T>();
         response.setCode(code);
-        response.setDescription(description);
-        response.setPayload(payload);
+        response.setDescription(description == null ? "" : description);
+        if (payload != null) {
+            response.setPayload(payload);
+        }
         return response;
     }
 }
