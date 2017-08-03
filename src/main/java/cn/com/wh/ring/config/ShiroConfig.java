0bebc1f5428653ec8b80b5b6dbfb9760720e329f
huihuicodesoft <836320956@qq.com>
2017-08-03 12:00:49 +0800

    修复@RequirePermissions不起作用

--- src/main/java/cn/com/wh/ring/config/ShiroConfig.java
+++ src/main/java/cn/com/wh/ring/config/ShiroConfig.java
@@ -5,9 +5,10 @@ import cn.com.wh.ring.app.auth.TokenRealm;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,6 +20,20 @@ import java.util.Collection;
  */
 @Configuration
 public class ShiroConfig {
+    @Bean
+    // Required for security annotations to work in this servlet
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator(){
+        DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
+        defaultAdvisorAutoProxyCreator.setProxyTargetClass(true);
+        return defaultAdvisorAutoProxyCreator;
+    }
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(){
+        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        return authorizationAttributeSourceAdvisor;
+    }
+
     @Bean
     public Realm tokenRealm() {
         return new TokenRealm();
